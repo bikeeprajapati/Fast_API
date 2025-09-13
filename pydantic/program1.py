e0a6959fa268face3fbbb10edbd2340be6efa561
Bikee Prajapati <bikeeprajapati1@gmail.com>
2025-09-13 23:23:05 +0545

    Implemented Data Validation

--- pydantic/program1.py
+++ pydantic/program1.py
@@ -1,19 +1,30 @@
-from pydantic import BaseModel
-from typing import List, Dict
+from pydantic import BaseModel, EmailStr,AnyUrl,Field
+from typing import List, Dict, Optional , Annotated
 
 class Patient(BaseModel):
-    name: str
+    name: Annotated[str, Field(min_length=1, max_length=100)]
+    LinkedIn: AnyUrl
+    email: EmailStr
     age: int
-    weight: float
-    married = bool
-    allergies: List[str]
+    weight: Annotated[float, Field(..., gt=0, strict=True,description="Weight must be a positive number")]
+    married: bool
+    allergies: Optional[List[str]] = None
     contact_details: Dict[str, str]
 
-patient_info ={'name': 'John Doe', 'age': 30 , 'weight': 70.5, 'married': False, 'allergies': ['peanuts', 'penicillin'], 'contact_details': {'phone': '123-456-7890', 'email': 'abc@gmail.com'}}    
+patient_info = {
+    'name': 'John Doe',
+    "LinkedIn": "https://www.linkedin.com/in/johndoe",
+    'age': 30,
+    'email': 'abc@gmail.com',
+    'weight': 70.5,
+    'married': False,
+    'allergies': ['peanuts', 'penicillin'],
+    'contact_details': {'phone': '123-456-7890'}
+}    
 
 patient1 = Patient(**patient_info)
 
 def insert_patient(patient: Patient):
     print(f"Inserting patient: {patient.name}, Age: {patient.age}")
-    
+
 insert_patient(patient1)
