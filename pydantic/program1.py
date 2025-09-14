f592f5829fd0e6353a7b2f4d8edcff9e7798fb77
Bikee Prajapati <bikeeprajapati1@gmail.com>
2025-09-14 22:42:49 +0545

    `Added custom validators and computed field to Patient model`

--- pydantic/program1.py
+++ pydantic/program1.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr,AnyUrl,Field
+from pydantic import BaseModel, EmailStr,AnyUrl,Field , field_validator,model_validator,computed_field
 from typing import List, Dict, Optional , Annotated
 
 class Patient(BaseModel):
@@ -10,21 +10,57 @@ class Patient(BaseModel):
     married: bool
     allergies: Optional[List[str]] = None
     contact_details: Dict[str, str]
-
+    
+    @field_validator('age',mode='after')
+    @classmethod
+    def validate_age(cls, value):
+        if 0 < value < 100:
+            return value
+        else:
+            raise ValueError("Age must be between 0 and 100")
+    
+    @field_validator('name')
+    @classmethod
+    def Upper_name(cls,value):
+        return value.upper()
+    
+    @field_validator('email')
+    @classmethod
+    def validate_email_domain(cls, value):
+        valid_domain =['ebl.com', 'gbl.com', 'nbl.com']
+        domain_name = value.split('@')[-1]
+        
+        if domain_name not in valid_domain:
+            raise ValueError(f"Email domain must be one of the following: {', '.join(valid_domain)}")
+        return value
+    
+    @model_validator(mode='after')
+    def valid_emergency_contact(cls,model):
+        if model.age >60  and 'emergency_contact' not in model.contact_details:
+            raise ValueError("Emergency contact is required for patients over 60")
+        return model
+    
+    @computed_field
+    @property
+    def BMI(self) -> float:
+        BMI = self.weight / ((self.age/100) ** 2)
+        return  round(BMI,2)
+    
+    
 patient_info = {
     'name': 'John Doe',
     "LinkedIn": "https://www.linkedin.com/in/johndoe",
-    'age': 30,
-    'email': 'abc@gmail.com',
+    'age': '78',
+    'email': 'abc@gbl.com',
     'weight': 70.5,
     'married': False,
     'allergies': ['peanuts', 'penicillin'],
-    'contact_details': {'phone': '123-456-7890'}
+    'contact_details': {'phone': '123-456-7890','emergency_contact':'987-654-3210'}
 }    
 
 patient1 = Patient(**patient_info)
 
 def insert_patient(patient: Patient):
-    print(f"Inserting patient: {patient.name}, Age: {patient.age}")
+    print(f"Inserting patient: {patient.name}, Age: {patient.age},BMI: {patient.BMI}")
 
 insert_patient(patient1)
